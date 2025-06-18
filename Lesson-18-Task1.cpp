a2baf01d1d9994ad029be9dab4a907d7720679e1
Ihor Kyrychenko <legouflandia@gmail.com>
2025-06-18 18:16:11 +0200

    V2.0

--- Lesson-18-Task1.cpp
+++ Lesson-18-Task1.cpp
@@ -2,5 +2,17 @@
 #include "Computer.h"
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n = 5;
+    computer* arr = new computer[n]{
+        {"asus", 8, 15'000},
+        { "lenovo", 9, 15'000},
+        {"hp", 8, 15'000}
+    };
+    cout << "computer with RAM > 10\n";
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i].ram > 10) {
+            cout << arr[i].name << endl;
+        }
+    }
 }
